ad4bc00f5540c8137d0e1227106000d3c5970701
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-17 23:33:13 +0600

    error message show

--- src/Components/ControlledForm/ControlledForm.jsx
+++ src/Components/ControlledForm/ControlledForm.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const ControlledForm = () => {
 
     const [password, setPassword]=useState("");
+    const [error, setError]=useState("");
 
     const handleControlledForm=(e)=>{
         e.preventDefault()
@@ -12,14 +13,27 @@ const ControlledForm = () => {
 
     const handlePasswordOnchange=e=>{
         console.log(e.target.value)
+        setPassword(e.target.value);
+
+        if(password.length < 6){
+            setError("password must be 6 character or longer");
+        }
+
+        else{
+            setError("")
+        }
     }
+
+
     return (
         <div>
             <form onSubmit={handleControlledForm}> 
                 <input type="email" name="email" placeholder='Email' required id="" /> <br />
                 <input type="password" onChange={handlePasswordOnchange} defaultValue={password} name="password" required placeholder='Password' id="" /> <br />
                 <input type="submit" value="submit" />
+                <p>{error}</p>
             </form>
+            
         </div>
     );
 };
